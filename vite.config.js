feb7ba5b85d6d52c6230ee100f8e8456f42cc5b2
Solilium <steven33152239@gmail.com>
2025-07-08 18:53:49 +0800

    fix: externalize react-native-web for Netlify build

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: [
+        'react-native-web',
+        'react-native-web/dist/apis/StyleSheet/registry'
+      ]
+    }
+  }
 })
